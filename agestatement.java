3fb913b7ebe63a14f3dff37e573f709a0c4dd173
gurkiratsingh1101 <gurkiratsingh1219@gmail.com>
2026-04-29 10:26:35 +0530

    Create agestatement.java

---
+++ agestatement.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class agesstatement {
+    public static void main(String[]args) {
+        int age = 56;
+        if (age>56) {
+            System.out.println("You are experienced");
+        }
+    
+    else {
+        System.out.println("You are not ");
+    }
+}
+}
