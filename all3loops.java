13cfc71d4e543bbe74a6652ffe34c24fc094e080
gurkiratsingh1101 <gurkiratsingh1219@gmail.com>
2026-04-29 10:32:53 +0530

    Create all3loops.java

---
+++ all3loops.java
@@ -0,0 +1,22 @@
+public class istloops {
+public static void main(String[]args) {
+        System.out.println("For loop:");
+        for(int i=1; i<=5; i++) {
+            System.out.println("Number:"+i);
+        }
+        System.out.println("\nWHile Loop");
+        int j = 1;
+        while(j<=5) {
+            System.out.println("Number:"+j);
+            j++;
+        }
+        System.out.println("\nDo-While Loop:");
+        int k = 1;
+        do {
+            System.out.println ("Number:"+k);
+            k++;
+        }
+        while(k<=5);
+    }
+    
+}
