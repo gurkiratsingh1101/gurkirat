a1c10ef45fa6b062243bf7cd8318d45d6cdcc80f
gurkiratsingh1101 <gurkiratsingh1219@gmail.com>
2026-04-29 10:29:57 +0530

    Add if-else structure to check driving age

---
+++ ifelse.java
@@ -0,0 +1,11 @@
+public class ifelse {
+    public static void main(String[]args) {
+        int age = 18;
+        if(age>18) {
+        System.out.println("You can drive");
+    }
+        else{
+            System.out.println("Not yet");
+        } 
+    }   
+}
